9bfaa66668f648cc587a785b534352ea51ea5d60
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-11-12 14:59:14 -0600

    data-serialization

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // referenced activity 1 from module 14 for code
 // defining dependencies
+const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const path = require("path");
 const handlebars = exphbs.create({});
-
 // set up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sequelize = require(".config/connection");
 
 // setting up Handlebars as the default template engine
 // establishing the engine
@@ -15,11 +15,13 @@ app.engine("handlebars", handlebars.engine);
 // setting handlebars as the default engine
 app.set("view engine", "handlebars");
 // establishing the path from the current directory to public
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 // setting the path routes
 app.use(require("./controllers/blog-routes"));
 
 // starts the server to begin listening
-app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
 });
